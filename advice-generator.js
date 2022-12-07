6c61f1bf1fd160183dd8b849f6c2f954d6020749
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2022-12-07 14:47:35 -0500

    connects api, needs to add button

---
+++ advice-generator.js
@@ -0,0 +1,13 @@
+const fetchAdvice = async () => {
+  // getting response from server
+  const res = await fetch("https://api.adviceslip.com/advice");
+  // getting a JSON string from the response from the server
+  const data = await res.json();
+  console.log(data);
+  document.getElementById(
+    "advice-number"
+  ).innerHTML = `Advice #${data.slip.id}`;
+  document.getElementById("advice").innerHTML = `"${data.slip.advice}"`;
+};
+
+fetchAdvice();
