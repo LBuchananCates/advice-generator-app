fe2dc4fc57fa1d7d53c90a7c4497de84312b5636
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2022-12-12 17:07:39 -0500

    uses fetch for api

--- advice-generator.js
+++ advice-generator.js
@@ -1,13 +1,42 @@
+// const req = new XMLHttpRequest();
+
+// req.onload = function () {
+//   console.log("IT LOADED!!!!");
+//   const data = JSON.parse(this.responseText);
+//   console.log(data.name, data.height);
+// };
+
+// req.onerror = function () {
+//   console.log("ERROR");
+//   console.log(this);
+// };
+
+// req.open("GET", "https://api.adviceslip.com/advice");
+// req.send();
+
+// fetch("https://api.adviceslip.com/advice") //fetch returns a promise, will send request to url
+//   .then((res) => {
+//     console.log("resolved!", res);
+//     res.json();
+//     return res.json; //returns promise
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((e) => {
+//     console.log("error!", e);
+//   });
+
+const adviceNum = document.getElementById("advice-number");
+const adviceQuote = document.getElementById("advice");
+
 const fetchAdvice = async () => {
-  // getting response from server
   const res = await fetch("https://api.adviceslip.com/advice");
-  // getting a JSON string from the response from the server
   const data = await res.json();
   console.log(data);
-  document.getElementById(
-    "advice-number"
-  ).innerHTML = `Advice #${data.slip.id}`;
-  document.getElementById("advice").innerHTML = `"${data.slip.advice}"`;
+
+  adviceNum.innerHTML = `Advice #${data.slip.id}`;
+  adviceQuote.innerHTML = `"${data.slip.advice}"`;
 };
 
 fetchAdvice();
